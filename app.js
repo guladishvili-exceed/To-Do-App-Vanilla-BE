c6a22e06b208330510d13513f37b1903695a1fd2
guladishvili-exceed <63454784+guladishvili-exceed@users.noreply.github.com>
2020-05-07 20:30:49 +0400

    Another summary
    Co-Authored-By: roman-exceed <roman-exceed@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
+let todoItems = [{
+    todo: 'Walk',
+    id : 1
+    }]
+
 const express = require('express')
 let app = new express();
 
 app.get('/',function(req,res){
-    res.send('hello from node sample')
+    
+    res.send('Welcome to my server -Roffybc')
 })
 
-app.get('/contact',function(req,res){
-    res.send('hello from node contact')
+app.get('/read',function(req,res){
+    console.log('This is the page which read everything')
+    res.send(todoItems)
 })
 
-app.get('/about',function(req,res){
-    res.send('hello from node about')
+app.get('/addNew',function(req,res){
+    let newArray = todoItems.push({todo:'Run',id:2})
+    res.send(todoItems)
 })
 
-app.get('/random',function(req,res){
-    res.send('hello from node random')
+app.get('/editObject',function(req,res){
+    let todoIndex = todoItems.findIndex((obj => obj.id == 1))
+    todoItems[todoIndex].todo = 'Code'
+    res.send(todoItems)
+})
+
+app.get('/deleteObject',function(req,res){
+    let todoIndex = todoItems.findIndex((obj => obj.id == 1))
+    todoItems = todoItems.splice[todoIndex]
+    res.send(todoItems)
 })
 
 app.listen(3000,function(){
