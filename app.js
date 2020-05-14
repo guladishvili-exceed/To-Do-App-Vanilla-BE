f9ba988ade7d954637d062f7d0f9b61aafc41eb5
guladishvili-exceed <63454784+guladishvili-exceed@users.noreply.github.com>
2020-05-14 10:01:36 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-
+//Create
 let todoItems = [{
     todo: 'Walk',
-    id : '1'
+    id : '0'
     },
     {
     todo:'Dance',
+    id :'1'
+    },{
+    todo:'Code',
     id : '2'
     }]
 
@@ -15,22 +18,36 @@ let app = new express();
 let bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
-app.get('/',function(req,res){
+//Read
+app.get('/',(req,res) => {
     console.log('Welcome to roffys server')
     res.send(todoItems)
 })
 
-app.post('/addNew',function(req,res){
-   console.log(req.body)
-   res.send('Post method is up!')
+//add new item
+app.post('/addNew/:todo/ololo/:id', (req, res) => {
+    todoItems.push(req.params);
+    res.send(todoItems);
+  });
+
+  //delete item
+app.post('/deleteItem/ololo/:id',(req,res) => {
+    let findID =parseInt(req.params.id)
+    let filteredTodo = todoItems.filter(item => item.id != findID)
+    console.log(filteredTodo)
+    res.send(filteredTodo)
+    
 })
 
-app.delete('/deleteObject',function(req,res){
-    let findItem = todoItems.findIndex(x => x.id === '2')
-    todoItems.splice(findItem)
+//Edit item 
+app.post('/editItem/ololo/:id',(req,res) => {
+    let findId = parseInt(req.params.id)
+    let findIndex = todoItems.findIndex(item => item.id == findId)
+    todoItems[findIndex].todo = 'Jump'  
     res.send(todoItems)
     
 })
+
 
 app.listen(3000,function(){
     console.log('server is up')
